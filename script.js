1a3da79330c14a1abe9352977aeda2f58e666444
arsmoriendy <nugrahasteven@gmail.com>
2023-03-16 08:00:23 +0700

    feat(ui): color, themes, header, title

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-window.addEventListener("load", e => {
+window.addEventListener("load", () => {
+  function updateTheme() {
+    if (lightMode) {
+      document.body.setAttribute("class", "light");
+    } else {
+      document.body.setAttribute("class", "dark");
+    }
+    localStorage["lightMode"] = lightMode;
+  };
+
+  let lightMode = localStorage["lightMode"] === undefined ? false : JSON.parse(localStorage["lightMode"]);
+  updateTheme();
+
+  const toggleThemeButton = document.querySelector("#toggleThemeButton");
+  toggleThemeButton.addEventListener("click", () => {
+    lightMode = !lightMode;
+    updateTheme();
+  });
+
   [... document.querySelectorAll("td.color")].forEach(td => {
     const color = td.textContent;
 
