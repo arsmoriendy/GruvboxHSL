e189c981c05a9953d27f281eb931fb410a9e2880
arsmoriendy <nugrahasteven@gmail.com>
2023-03-18 11:23:57 +0700

    feat: fonts, options
    - updated font to "Caskaydia Cove Mono" nerd font(for icons, etc)
    - added options div to wrap over theme option and new show table option

--- script.js
+++ script.js
@@ -1,22 +1,106 @@
 window.addEventListener("load", () => {
-  function updateTheme() {
-    if (lightMode) {
-      document.body.setAttribute("class", "light");
-    } else {
-      document.body.setAttribute("class", "dark");
-    }
-    localStorage["lightMode"] = lightMode;
-  };
+  addCopyButtons();
 
-  let lightMode = localStorage["lightMode"] === undefined ? false : JSON.parse(localStorage["lightMode"]);
-  updateTheme();
-
-  const toggleThemeButton = document.querySelector("#toggleThemeButton");
-  toggleThemeButton.addEventListener("click", () => {
-    lightMode = !lightMode;
-    updateTheme();
+  // toggle options
+  const options = document.querySelector(".options");
+  const toggleOptionsButton = document.querySelector(".options>button");
+  const optionsModal = document.querySelector(".options>.modal");
+  [toggleOptionsButton, optionsModal].forEach(element => {
+    element.addEventListener("click", () => toggleActive(options));
   });
 
+  // toggle dropdowns
+  [... document.querySelectorAll(".dropdownOption")].forEach(element => {
+    element.querySelector(".dropdownOptionButton").addEventListener("click", () => toggleActive(element));
+  })
+
+  let lightMode = localStorage["lightMode"] === undefined ? false : JSON.parse(localStorage["lightMode"]);
+  updateTheme(lightMode);
+
+  // toggle theme
+  const toggleTheme = document.querySelector("#toggleTheme");
+  toggleTheme.addEventListener("click", e => {
+    lightMode = !lightMode;
+    updateTheme(lightMode);
+    e.target.checked = lightMode;
+  });
+
+  // toggle tables
+  const darkTable = document.querySelector("#darkTable");
+  const lightTable = document.querySelector("#lightTable");
+  const toggleShowDarkTable = document.querySelector('#toggleShowDarkTable');
+  const toggleShowLightTable = document.querySelector('#toggleShowLightTable');
+
+  // initialize
+  if (localStorage["showTable"]) {
+    const showTable = JSON.parse(localStorage["showTable"]);
+    if (showTable[0]) addActive(darkTable);
+    if (showTable[1]) addActive(lightTable);
+  } else {
+    addActive(darkTable);
+    addActive(lightTable);
+  }
+
+  function updateTableChecksAndLocalStorage() {
+    const showTable = [];
+    showTable[0] = toggleShowDarkTable.checked = checkActive(darkTable);
+    showTable[1] = toggleShowLightTable.checked = checkActive(lightTable);
+    localStorage["showTable"] = JSON.stringify(showTable);
+  }
+  updateTableChecksAndLocalStorage();
+
+  toggleShowDarkTable.addEventListener("click", () => {
+    if (checkActive(lightTable)) {
+      toggleActive(darkTable);
+    } else if (!checkActive(darkTable)) {
+      addActive(darkTable);
+    } else {
+      removeActive(darkTable);
+      addActive(lightTable);
+    }
+    updateTableChecksAndLocalStorage();
+  })
+  toggleShowLightTable.addEventListener("click", () => {
+    if (checkActive(darkTable)) {
+      toggleActive(lightTable);
+    } else if (!checkActive(lightTable)) {
+      addActive(lightTable);
+    } else {
+      removeActive(lightTable);
+      addActive(darkTable);
+    }
+    updateTableChecksAndLocalStorage();
+  })
+})
+
+function checkActive(element) {
+  return element.classList.contains("active");
+}
+
+function addActive(element) {
+  element.classList.add("active");
+}
+
+function removeActive(element) {
+  element.classList.remove("active");
+}
+
+function toggleActive(element) {
+  if (checkActive(element)) {
+    removeActive(element);
+  } else {
+    addActive(element);
+  }
+}
+
+function updateTheme(lightMode) {
+  lightMode
+    ? document.body.setAttribute("class", "light")
+    : document.body.setAttribute("class", "dark");
+  localStorage["lightMode"] = lightMode;
+};
+
+function addCopyButtons() {
   [... document.querySelectorAll("td.color")].forEach(td => {
     const color = td.textContent;
 
@@ -28,4 +112,5 @@ window.addEventListener("load", () => {
     copyBtn.innerText = "Copy"
     td.appendChild(copyBtn)
   })
-})
+}
+
