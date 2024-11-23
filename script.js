a2ef345cfc665f9e8a7a6397736cabf1051a5075
arsmoriendy <nugrahasteven@gmail.com>
2024-11-23 22:38:07 +0700

    feat: redone fonts and icons

--- script.js
+++ script.js
@@ -177,14 +177,17 @@ function updateTheme(lightMode) {
   localStorage["lightMode"] = lightMode;
 }
 
+const checkSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-check"><circle cx="12" cy="12" r="10"/><path d="m9 12 2 2 4-4"/></svg>`;
+const copySVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>`;
+
 function addCopyButton(td) {
   const copyBtn = document.createElement("button");
   copyBtn.setAttribute("class", "copyBtn");
-  copyBtn.innerText = "󱉨";
+  copyBtn.innerHTML = copySVG;
   copyBtn.addEventListener("click", (e) => {
-    e.target.innerText = "";
+    e.target.innerHTML = checkSVG;
     setTimeout(() => {
-      e.target.innerText = "󱉨";
+      e.target.innerHTML = copySVG;
     }, 5000);
     navigator.clipboard.writeText(
       e.target.parentElement.childNodes[0].nodeValue.trim(),
